d643dd4e6aea24ee0db23cea532edbbffb0d6621
imrankhanf41 <imranbutex41@gmail.com>
2021-10-20 21:32:17 +0600

    last commit

--- src/components/Home/Login1/Login1.js
+++ src/components/Home/Login1/Login1.js
@@ -1,21 +1,11 @@
 import React from 'react';
-import { Link,useLocation} from 'react-router-dom';
-import {useHistory} from "react-router-dom";
+import { Link} from 'react-router-dom';
 import useAuth from '../../../hooks/useAuth';
 
 
 const Login1 = () => {
     const{ signInUsingGoggle}=useAuth();
-   const location =useLocation();
-   const history =useHistory();
-   console.log("came from",location.state?.from)
-   const redirect_uri =location.state?.from || '/home'
-    const handleGoogleLogin =()=>{
-        signInUsingGoggle()
-        .then(result=>{
-            history.push(redirect_uri);
-        })
-    }
+
     return (
         <div className="mt-5">
             <h3 className="pt-5">Please Log In</h3>
@@ -25,7 +15,7 @@ const Login1 = () => {
         <button className="login button">Log in</button>
         <p className="text">New User?<Link to="/registration">Create An Account?</Link></p>
         <div>Or</div>
-            <button onClick={handleGoogleLogin}  className="login button">Google Sign In</button>
+            <button onClick={signInUsingGoggle}  className="login button">Google Sign In</button>
         
         </form>
         </div>
--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -4,29 +4,43 @@ import initializeAuthentication from "../Firebase/firebase.init";
 initializeAuthentication();
 const useFirebase=()=>{
     const [user,setUser]=useState({});
+    const [isLoading,setIsLoading]=useState(true);
     const auth=getAuth();
     const googleProvider = new GoogleAuthProvider();
     const signInUsingGoggle=()=>{
-       return signInWithPopup(auth,googleProvider);
+        setIsLoading(true)
+       signInWithPopup(auth,googleProvider)
+       .then(result=>{
+              setUser(result.user)
+        })
+        .finally(()=>setIsLoading(false));
         
     }
     const logOut =()=>{
+        setIsLoading(true);
         signOut(auth)
         .then(() => {
            setUser({})
           })
+          .finally(()=>setIsLoading(false));
     }
     useEffect(()=>{
-        onAuthStateChanged(auth, (user) => {
+       const unsubscribed = onAuthStateChanged(auth, (user) => {
             if (user) {
              setUser(user)
             } 
+            else{
+                setUser({})
+            }
+            setIsLoading(false)
           });
+          return ()=> unsubscribed;
     },[])
     return {
         user,
         signInUsingGoggle,
-        logOut,signInWithPopup
+        logOut,signInWithPopup,
+        isLoading
     }
 }
 export default useFirebase;
